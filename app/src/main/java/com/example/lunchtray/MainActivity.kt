2e27c6356ca6443c9f0ea42fba1ed9543960213f
Roy Sylvanus <66891435+vivakenya@users.noreply.github.com>
2022-01-14 20:23:01 +0100

    Modified Main Activity by initializing navController

--- app/src/main/java/com/example/lunchtray/MainActivity.kt
+++ app/src/main/java/com/example/lunchtray/MainActivity.kt
@@ -18,6 +18,7 @@ package com.example.lunchtray
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import com.example.lunchtray.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -32,6 +33,9 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val navHost = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        navController = navHost.navController
 
         // TODO: Retrieve NavController from the NavHostFragment
     }
